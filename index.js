0eeaab5cd7d2fc7c43a9c279f66f79783860d3af
Mathias Buus <mathiasbuus@gmail.com>
2018-01-31 11:12:13 +0100

    get rid of extra shell

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 var proc = require('child_process')
+var path = require('path')
 
 module.exports = spawn
 
 function spawn (flags, cmd, opts) {
-  return proc.spawn('sh', ['-c', `
-    NODE_BIN="$(which node)"
-    PATH="${__dirname}/bin:$PATH"
-    NITM_BIN="$NODE_BIN" NITM_FLAGS="${flags.join(' ')}" ${stringify(cmd)}
-  `], opts)
-}
-
-function stringify (arr) {
-  return arr.map(item => JSON.stringify(item)).join(' ')
+  return proc.spawn(cmd[0], cmd.slice(1), {
+    env: Object.assign({}, process.env, {
+      NITM_BIN: process.execPath,
+      NITM_FLAGS: flags.join(' '),
+      PATH: path.join(__dirname, 'bin') + ':' + process.env.PATH
+    })
+  })
 }
